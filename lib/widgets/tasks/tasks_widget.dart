06fdccd01e2401c529c5e863a1a1c9eabd23a0dc
neketta300 <neketta300@gmail.com>
2025-03-09 11:02:43 +0300

    add list of tasks

--- lib/widgets/tasks/tasks_widget.dart
+++ lib/widgets/tasks/tasks_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:llf_todo_app/widgets/tasks/tasks_widget_model.dart';
 
 class TasksWidget extends StatefulWidget {
@@ -43,8 +44,66 @@ class TaskWidgetBody extends StatelessWidget {
         title: Text(title),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () => model?.showForm(context),
         child: Icon(Icons.add),
+      ),
+      body: _TaskListWidget(),
+    );
+  }
+}
+
+class _TaskListWidget extends StatelessWidget {
+  const _TaskListWidget();
+
+  @override
+  Widget build(BuildContext context) {
+    final int groupsCount =
+        TasksWidgetModelProvider.watch(context)?.model.tasks.length ?? 0;
+    return ListView.separated(
+      itemCount: groupsCount,
+      separatorBuilder: (BuildContext context, int index) {
+        return Divider(
+          height: 1,
+        );
+      },
+      itemBuilder: (BuildContext context, int index) {
+        return _TasksListRowWidget(
+          taskListIndex: index,
+        );
+      },
+    );
+  }
+}
+
+class _TasksListRowWidget extends StatelessWidget {
+  final int taskListIndex;
+  const _TasksListRowWidget({required this.taskListIndex});
+
+  @override
+  Widget build(BuildContext context) {
+    final model = TasksWidgetModelProvider.watch(context)!.model;
+    final task = model.tasks[taskListIndex];
+    return Slidable(
+      endActionPane: ActionPane(
+        motion: BehindMotion(),
+        children: [
+          SlidableAction(
+            onPressed: (BuildContext context) =>
+                model.deleteTask(taskListIndex),
+            backgroundColor: Color.fromARGB(255, 223, 16, 16),
+            foregroundColor: Colors.white,
+            icon: Icons.delete,
+            label: 'Удалить',
+          ),
+        ],
+      ),
+      child: ListTile(
+        title: Text(
+          task.text,
+          style: TextStyle(color: Colors.black),
+        ),
+        trailing: Icon(Icons.chevron_right),
+        onTap: () {},
       ),
     );
   }
